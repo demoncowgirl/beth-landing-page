98cfdba8a65f2f36c3919a64965e8ee7f4a68f9e
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-02 15:46:39 -0400

    adds background to landing page

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,25 @@ gulp.task('copycss', function() {
 		.pipe(gulp.dest('dist/'))
 });
 
+gulp.task('sass', function(){
+  return gulp.src('app/css/*.scss')
+    .pipe(gulp.dest('dist/'))
+});
+
+// function to change sas
+gulp.task('copyimages', function() {
+	return gulp.src('app/images/*.+(jpg|jpeg|png|gif)')
+		.pipe(gulp.dest('dist/images/'))
+});
+
 gulp.task('watch', function() {
 	gulp.watch('app/*.+(htm|html)', gulp.series('copyhtml'));
   gulp.watch('app/**/*.css', gulp.series('copycss'));
+  gulp.watch('app/**/*.scss', gulp.series('sass'));
+  gulp.watch('app/**/*.+(jpg|jpeg|png|gif)', gulp.series('copyimages'));
+
 });
 
 gulp.task('default', function(callback) {
-	runSequence(['sass', 'copycss', 'copyjs'], callback)
+	runSequence(['sass', 'copycss', 'copyjs', 'copyimages'], callback)
 });
--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-function openVert(){
-  window.open('https://beth-js-weather-app.herokuapp.com/', width=200,height=850);
-}
 
-function openHoriz(){
-  window.open('https://beth-js-weather-app.herokuapp.com/', width=850,height=200);
-}
